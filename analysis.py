dac410569f884363e03f463cf13953624a276926
Ivan Jasjko <i.jasjko@gmail.com>
2017-04-08 12:52:27 +0100

    URLs replaced to remove duplicates (regex).

--- analysis.py
+++ analysis.py
@@ -1,6 +1,5 @@
 import pandas
 import csv
-import re
 from sklearn.feature_extraction.text import CountVectorizer
 from sklearn.cross_validation import train_test_split
 from sklearn.naive_bayes import MultinomialNB
